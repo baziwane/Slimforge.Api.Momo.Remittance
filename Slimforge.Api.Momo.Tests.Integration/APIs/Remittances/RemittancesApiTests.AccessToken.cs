ff464d33376f4af530d91a8a08db4a281f265e91
baziwane <raymond.baziwane@gmail.com>
2023-11-29 09:47:59 -0600

    TESTS: Initialize Integration test project

---
+++ Slimforge.Api.Momo.Tests.Integration/APIs/Remittances/RemittancesApiTests.AccessToken.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------------------------- 
+// Copyright (c) The Standard Organization, a coalition of the Good-Hearted Engineers 
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Slimforge.Api.Momo.Models.Services.Foundations.Remittances;
+using Xunit;
+
+namespace Slimforge.Api.Momo.Tests.Integration.APIs.Remittances
+{
+    public partial class RemittancesApiTests
+    {
+        [Fact(Skip = "This test is only for releases")]
+        private async Task ShouldGenerateAccessTokenAsync()
+        {
+            // given - not needed
+            
+            // when
+            var responseAccessToken =
+                await this.momoClient.AccessTokens.GenerateAccessTokenAsync();
+
+            // then
+            Assert.NotNull(responseAccessToken.AccessToken);
+        }
+    }
+}
