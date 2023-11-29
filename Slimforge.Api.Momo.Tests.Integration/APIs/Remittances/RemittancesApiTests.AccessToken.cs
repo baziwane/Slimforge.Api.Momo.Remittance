87336dc774518145be6171c24521bf21ca6eaf22
baziwane <raymond.baziwane@gmail.com>
2023-11-29 11:13:11 -0600

    Update to skip integration tests

--- Slimforge.Api.Momo.Tests.Integration/APIs/Remittances/RemittancesApiTests.AccessToken.cs
+++ Slimforge.Api.Momo.Tests.Integration/APIs/Remittances/RemittancesApiTests.AccessToken.cs
@@ -10,7 +10,7 @@ namespace Slimforge.Api.Momo.Tests.Integration.APIs.Remittances
 {
     public partial class RemittancesApiTests
     {
-        [Fact]
+        [Fact(Skip = "This test is only for releases")]
         private async Task ShouldGenerateAccessTokenAsync()
         {
             // given - not needed
